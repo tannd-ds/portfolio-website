128ad828ce3fac5dd629b969a8594518574ea906
tannd-ds <donhattan1012dev@gmail.com>
2022-02-01 14:51:46 +0700

    player names & random roles

--- js/werewolf.js
+++ js/werewolf.js
@@ -1,12 +1,20 @@
-
+// Displayed Variables
 let clickMe = document.querySelector('button');
 let randomNumberOutput = document.querySelector('h1.random')
+
+// Data-contained Variables
+let gameStarted = false;
+let numbersArray;
+
+let characters = new Array('Dân Làng', 'Dân Làng', 'Dân Làng', 'Sói', 'Sói');
+let players = [];
 
 function getRandomNumber(min, max) {
 	let totalEle = max - min + 1;
 	let result = Math.floor(Math.random() * totalEle) + min;
 	return result;
 }
+
 function createArrayOfNumber(start, end) {
 	let myArray = [];
 	for (let i = start; i <= end; i++) {
@@ -14,18 +22,40 @@ function createArrayOfNumber(start, end) {
 	}
 	return myArray;
 }
-let numberOfPlayers = prompt("How many players?");
-let numbersArray = createArrayOfNumber(1, numberOfPlayers);
+
 
 clickMe.addEventListener('click', () => {
-	if (numbersArray.length === 0) {
-		console.log('No more random number');
-		return;
+	if (!gameStarted) {
+		let numberOfPlayers = prompt("Có bao nhiêu người chơi vậy ạ?");
+		numbersArray = createArrayOfNumber(0, numberOfPlayers-1);
+		gameStarted = true;
+
+		// Minor Display changes
+		randomNumberOutput.innerHTML = `Chuyền Máy Cho Người Chơi Đầu Tiên`;
+	} else {
+		if (numbersArray.length === 0) {
+			console.log('No more random number');
+			console.log(players);
+			return;
+		}
+
+		randomNumberOutput.innerHTML = `Chuyền Máy Cho Người Chơi Tiếp Theo`;
+
+		let currentPlayer = prompt("Xin cái Tên đê");
+
+		let randomIndex = getRandomNumber(0, numbersArray.length - 1);
+		let randomNumber = numbersArray[randomIndex];
+
+		numbersArray.splice(randomIndex, 1);
+
+		console.log(randomNumber);
+
+		randomNumberOutput.innerHTML = `${currentPlayer} là ${characters[randomNumber]}`;
+
+		players.push({
+			Name: currentPlayer,
+			Role: characters[randomNumber],
+		})
 	}
-	let randomIndex = getRandomNumber(0, numbersArray.length - 1);
-	let randomNumber = numbersArray[randomIndex];
-	numbersArray.splice(randomIndex, 1);
-	console.log(randomNumber);
-	let characters = new Array('Dân Làng', 'Dân Làng', 'Dân Làng', 'Sói', 'Sói');
-	randomNumberOutput.innerHTML = `You're ${characters[randomNumber]}`;
+
 });
